5df9364e4fd3cb40ff86c8a192621ad054a9a6cb
akai-z <akai-z@users.noreply.github.com>
2018-08-25 21:31:47 +0300

    Move Varnish repo add script into a function

--- context/src/scripts/docker-varnish-repo.sh
+++ context/src/scripts/docker-varnish-repo.sh
@@ -11,37 +11,42 @@ readonly GPGKEY_FILE="docker_varnish_gpgkey"
 readonly GPGKEY_PUB_LABEL="pub:-:"
 readonly GNUPG_DIR="/root/.gnupg"
 
-if [ ! -d $GNUPG_DIR ]; then
-  mkdir $GNUPG_DIR
-  chmod 700 $GNUPG_DIR
-fi
+function add()
+{
+  if [ ! -d $GNUPG_DIR ]; then
+    mkdir $GNUPG_DIR
+    chmod 700 $GNUPG_DIR
+  fi
 
-curl -fsSL -o $GPGKEY_FILE $GPGKEY_URL
+  curl -fsSL -o $GPGKEY_FILE $GPGKEY_URL
 
-gpgkey="$( \
-  gpg -q \
-    --dry-run \
-    --with-colons \
-    --import-options import-show \
-    --import $GPGKEY_FILE \
-)"
+  gpgkey="$( \
+    gpg -q \
+      --dry-run \
+      --with-colons \
+      --import-options import-show \
+      --import $GPGKEY_FILE \
+  )"
 
-echo "$gpgkey" \
-  | grep -q $GPGKEY_FINGERPRINT \
-  || exit 1 # Wrong/Malicious key.
-
-gpgkeys_count=$( \
   echo "$gpgkey" \
-  | grep -c "^${GPGKEY_PUB_LABEL}" \
-)
+    | grep -q $GPGKEY_FINGERPRINT \
+    || exit 1 # Wrong/Malicious key.
 
-if [ $gpgkeys_count -gt 1 ]; then
-  exit 1 # Malicious key.
-fi
+  gpgkeys_count=$( \
+    echo "$gpgkey" \
+    | grep -c "^${GPGKEY_PUB_LABEL}" \
+  )
 
-apt-key add $GPGKEY_FILE
+  if [ $gpgkeys_count -gt 1 ]; then
+    exit 1 # Malicious key.
+  fi
 
-echo "deb $REPO_URL $(lsb_release -cs) main" \
-  > /etc/apt/sources.list.d/docker_varnish.list
+  apt-key add $GPGKEY_FILE
 
-rm -rf $GPGKEY_FILE $GNUPG_DIR
+  echo "deb $REPO_URL $(lsb_release -cs) main" \
+    > /etc/apt/sources.list.d/docker_varnish.list
+
+  rm -rf $GPGKEY_FILE $GNUPG_DIR
+}
+
+add

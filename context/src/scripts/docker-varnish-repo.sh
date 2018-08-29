c26004af60ff3e0cf21ccc875752296f0afd7f68
akai-z <akai-z@users.noreply.github.com>
2018-08-29 03:54:48 +0300

    Refactor Varnish repo shell script add function and split it into smaller functions

--- context/src/scripts/docker-varnish-repo.sh
+++ context/src/scripts/docker-varnish-repo.sh
@@ -22,24 +22,60 @@ add()
   local gpgkey_url="${repo_base_url}/gpgkey"
   local gpgkey_pub_label="pub:-:"
 
+  install_fetch_deps
+  create_gnupg_dir
+  fetch_gpgkey_file $gpgkey_url
+  verify_gpgkey $gpgkey_fingerprint $gpgkey_pub_label
+  add_gpgkey_to_trusted_keys_list
+
+  echo "deb $repo_url $(lsb_release -cs) main" \
+    > $SOURCE_LIST_FILE
+}
+
+clean()
+{
+  apt-get purge -y --auto-remove $FETCH_DEPS
+  rm -rf $GPGKEY_FILE $GNUPG_DIR $SOURCE_LIST_FILE
+}
+
+install_fetch_deps()
+{
   apt-get update
   apt-get install --no-install-recommends --no-install-suggests -y \
     $FETCH_DEPS
+}
 
+create_gnupg_dir()
+{
   if [ ! -d $GNUPG_DIR ]; then
     mkdir $GNUPG_DIR
     chmod 700 $GNUPG_DIR
   fi
+}
 
+fetch_gpgkey_file()
+{
+  local gpgkey_url="$1"
   curl -fsSL -o $GPGKEY_FILE $gpgkey_url
+}
 
-  gpgkey="$( \
+get_gpgkey()
+{
+  echo "$( \
     gpg -q \
       --dry-run \
       --with-colons \
       --import-options import-show \
       --import $GPGKEY_FILE \
   )"
+}
+
+verify_gpgkey()
+{
+  local gpgkey_fingerprint="$1"
+  local gpgkey_pub_label="$2"
+  local gpgkey="$(get_gpgkey)"
+  local gpgkeys_count
 
   echo "$gpgkey" \
     | grep -q $gpgkey_fingerprint \
@@ -50,20 +86,14 @@ add()
     | grep -c "^${gpgkey_pub_label}" \
   )
 
-  if [ $gpgkeys_count -gt 1 ]; then
+  if [ $gpgkeys_count -ne 1 ]; then
     exit 1 # Malicious key.
   fi
-
-  apt-key add $GPGKEY_FILE
-
-  echo "deb $repo_url $(lsb_release -cs) main" \
-    > $SOURCE_LIST_FILE
 }
 
-clean()
+add_gpgkey_to_trusted_keys_list()
 {
-  apt-get purge -y --auto-remove $FETCH_DEPS
-  rm -rf $GPGKEY_FILE $GNUPG_DIR $SOURCE_LIST_FILE
+  apt-key add $GPGKEY_FILE
 }
 
 main()
